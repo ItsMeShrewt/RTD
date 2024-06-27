634defd84f67d123136165c89876c04042e551f6
Richie Dadubo <tomanjiro13@gmail.com>
2024-06-27 11:41:22 +0800

    New Feats

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const nav = document.querySelector(".nav-links");
 burger.addEventListener("click", () => {
   nav.classList.toggle("nav-active");
 });
+
+document.querySelectorAll(".nav-links a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+
+    // Close the nav on small screens when a link is clicked
+    if (window.innerWidth <= 768) {
+      nav.classList.remove("nav-active");
+    }
+  });
+});
